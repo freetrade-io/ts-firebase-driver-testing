03dd3300a4779bdb5d2a4d0ffd60fd969255c6dc
Hugh Grigg <hugh@freetrade.io>
2019-09-29 21:29:50 +0100

    Cascading onCreate trigger test

--- tests/driver/RealtimeDatabase/Trigger/InProcessRealtimeDb.onCreate.test.ts
+++ tests/driver/RealtimeDatabase/Trigger/InProcessRealtimeDb.onCreate.test.ts
@@ -243,4 +243,217 @@ describe("onCreate trigger of in-process realtime database", () => {
             params: { animalName: "tiger" },
         })
     })
+
+    test("onCreate trigger with multiple path parameters", async () => {
+        // Given we set up an onCreate handler with multiple path parameters;
+        let receivedChange: IChangeSnapshots | undefined
+        let receivedContext: IChangeContext | undefined
+        driver
+            .runWith()
+            .database.ref("/animals/{animalName}/{featureName}")
+            .onCreate(async (change, context) => {
+                receivedChange = change
+                receivedContext = context
+            })
+
+        // When that path is created;
+        await driver
+            .realTimeDatabase()
+            .ref("/animals/tiger")
+            .set({
+                colour: "orange",
+            })
+
+        // And Firebase finishes its jobs;
+        await driver.jobsComplete()
+
+        // Then the handler should be triggered with the change and context.
+        expect(receivedChange!).toBeTruthy()
+        expect(receivedChange!.before.exists()).toBeFalsy()
+        expect(receivedChange!.before.val()).toBeUndefined()
+        expect(receivedChange!.after.exists()).toBeTruthy()
+        expect(receivedChange!.after.val()).toEqual("orange")
+
+        expect(receivedContext!).toBeTruthy()
+        expect(receivedContext!).toEqual({
+            params: { animalName: "tiger", featureName: "colour" },
+        })
+    })
+
+    test("multiple onCreate triggers", async () => {
+        // Given we set up an onCreate handler on a path;
+        const receivedChanges: IChangeSnapshots[] = []
+        const receivedContexts: IChangeContext[] = []
+        driver
+            .runWith()
+            .database.ref("/animals/{animalName}/sound")
+            .onCreate(async (change, context) => {
+                receivedChanges.push(change)
+                receivedContexts.push(context)
+            })
+
+        // When multiple such paths are created;
+        await driver
+            .realTimeDatabase()
+            .ref("/animals")
+            .set({
+                cat: {
+                    likes: "mice",
+                    sound: "meow",
+                },
+                dog: {
+                    likes: "sticks",
+                    sound: "woof",
+                },
+                pig: {
+                    likes: "everything",
+                    sound: "oink",
+                },
+            })
+
+        // And Firebase finishes its jobs;
+        await driver.jobsComplete()
+
+        // Then the handler should be triggered with the changes and contexts.
+        expect(receivedChanges).toHaveLength(3)
+        expect(receivedChanges[0].before.val()).toBeUndefined()
+        expect(receivedChanges[0].after.val()).toEqual("meow")
+        expect(receivedChanges[1].before.val()).toBeUndefined()
+        expect(receivedChanges[1].after.val()).toEqual("woof")
+        expect(receivedChanges[2].before.val()).toBeUndefined()
+        expect(receivedChanges[2].after.val()).toEqual("oink")
+
+        expect(receivedContexts).toHaveLength(3)
+        expect(receivedContexts[0]).toEqual({
+            params: { animalName: "cat" },
+        })
+        expect(receivedContexts[1]).toEqual({
+            params: { animalName: "dog" },
+        })
+        expect(receivedContexts[2]).toEqual({
+            params: { animalName: "pig" },
+        })
+    })
+
+    test("multiple onCreate triggers with multiple path parameters", async () => {
+        // Given we set up an onCreate handler with multiple path parameters;
+        const receivedChanges: IChangeSnapshots[] = []
+        const receivedContexts: IChangeContext[] = []
+        driver
+            .runWith()
+            .database.ref("/animals/{animalName}/{featureName}")
+            .onCreate(async (change, context) => {
+                receivedChanges.push(change)
+                receivedContexts.push(context)
+            })
+
+        // When multiple such paths are created;
+        await driver
+            .realTimeDatabase()
+            .ref("/animals")
+            .set({
+                cat: {
+                    likes: "mice",
+                    sound: "meow",
+                },
+                dog: {
+                    likes: "sticks",
+                    sound: "woof",
+                },
+                pig: {
+                    likes: "everything",
+                    sound: "oink",
+                },
+            })
+
+        // And Firebase finishes its jobs;
+        await driver.jobsComplete()
+
+        // Then the handler should be triggered with the changes and contexts.
+        expect(receivedChanges).toHaveLength(6)
+        expect(receivedChanges[0].after.val()).toEqual("mice")
+        expect(receivedChanges[1].after.val()).toEqual("meow")
+        expect(receivedChanges[2].after.val()).toEqual("sticks")
+        expect(receivedChanges[3].after.val()).toEqual("woof")
+        expect(receivedChanges[4].after.val()).toEqual("everything")
+        expect(receivedChanges[5].after.val()).toEqual("oink")
+
+        expect(receivedContexts).toHaveLength(6)
+        expect(receivedContexts[0]).toEqual({
+            params: { animalName: "cat", featureName: "likes" },
+        })
+        expect(receivedContexts[1]).toEqual({
+            params: { animalName: "cat", featureName: "sound" },
+        })
+        expect(receivedContexts[2]).toEqual({
+            params: { animalName: "dog", featureName: "likes" },
+        })
+        expect(receivedContexts[3]).toEqual({
+            params: { animalName: "dog", featureName: "sound" },
+        })
+        expect(receivedContexts[4]).toEqual({
+            params: { animalName: "pig", featureName: "likes" },
+        })
+        expect(receivedContexts[5]).toEqual({
+            params: { animalName: "pig", featureName: "sound" },
+        })
+    })
+
+    test("cascading onCreate triggering", async () => {
+        // Given we set up an onCreate handler on a path, that writes to another
+        // path.
+        driver
+            .runWith()
+            .database.ref("/animals/{animalName}/sound")
+            .onCreate(async (change, context) => {
+                await driver
+                    .realTimeDatabase()
+                    .ref(`/animal_sounds/${context.params.animalName}`)
+                    .set(change.after.val())
+            })
+
+        // And there is also an onCreate handler for that other path, which
+        // writes to a third path.
+        driver
+            .runWith()
+            .database.ref("/animal_sounds/{animalName}")
+            .onCreate(async (change, context) => {
+                await driver
+                    .realTimeDatabase()
+                    .ref(`/sound_to_animal/${change.after.val()}`)
+                    .set(context.params.animalName)
+            })
+
+        // When we write to the first path;
+        await driver
+            .realTimeDatabase()
+            .ref("/animals/tiger")
+            .set({
+                colour: "orange",
+                size: "large",
+                sound: "purr",
+            })
+
+        // And Firebase finishes its jobs;
+        await driver.jobsComplete()
+
+        // Then all the cascading writes should be made.
+        const firstWrite = (await driver
+            .realTimeDatabase()
+            .ref("animals/tiger/sound")
+            .once()).val()
+        expect(firstWrite).toEqual("purr")
+
+        const secondWrite = (await driver
+            .realTimeDatabase()
+            .ref("/animal_sounds/tiger")
+            .once()).val()
+        expect(secondWrite).toEqual("purr")
+
+        const thirdWrite = (await driver
+            .realTimeDatabase()
+            .ref("/sound_to_animal/purr")
+            .once()).val()
+        expect(thirdWrite).toEqual("tiger")
+    })
 })
