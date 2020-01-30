7d6a9431504b644bdaa3dd3a4d6bc6e8581ea594
Chris Pavlicek <chrisp@freetrade.io>
2020-01-30 18:25:19 +0000

    add details to error

--- src/driver/Firestore/FirestoreError.ts
+++ src/driver/Firestore/FirestoreError.ts
@@ -1,7 +1,11 @@
 import { GRPCStatusCode } from "../Common/GRPCStatusCode"
 
 export class FirestoreError extends Error {
+    details: string
     constructor(public code: GRPCStatusCode, message?: string) {
         super(`${code} ${GRPCStatusCode[code]}${message ? `: ${message}` : ""}`)
+        this.details = `${code} ${GRPCStatusCode[code]}${
+            message ? `: ${message}` : ""
+        }`
     }
 }
