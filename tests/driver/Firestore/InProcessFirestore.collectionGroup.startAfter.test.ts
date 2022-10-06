b852a53bd3499996183e127cb8a6ed1608662641
Chris Pavlicek <chris@varsisstudio.com>
2022-10-06 12:13:57 +0100

    fix: include document snapshot

--- tests/driver/Firestore/InProcessFirestore.collectionGroup.startAfter.test.ts
+++ tests/driver/Firestore/InProcessFirestore.collectionGroup.startAfter.test.ts
@@ -168,4 +168,46 @@ describe("In-process Firestore start after query on collectionGroup", () => {
         // Then we should get no items
         expect(result.size).toEqual(0)
     })
+
+    test("startAfter document id using documnet snapshot", async () => {
+        // Given there is a collection of documents with ids;
+        await db.doc("livingthings/animals/22da618d").set({ name: "aardvark" })
+        await db.doc("livingthings/animals/00a3382").set({ name: "badger" })
+        await db.doc("livingthings/animals/11cbe6b5").set({ name: "camel" })
+
+        const startAfterDoc = await db
+            .doc("livingthings/animals/11cbe6b5")
+            .get()
+
+        // When we order the collection by the document id;
+        const result = await db
+            .collectionGroup("animals")
+            .orderBy(FieldPath.documentId())
+            .startAfter(startAfterDoc)
+            .get()
+
+        // Then we should get the collection ordered by that field.
+        expect(result.size).toEqual(1)
+        expect(
+            result.docs.map((doc) => ({ id: doc.id, data: doc.data() })),
+        ).toStrictEqual([{ id: "22da618d", data: { name: "aardvark" } }])
+    })
+
+    test("startAfter document id after using document snapshot", async () => {
+        // Given there is a collection of documents with ids;
+        await db.doc("livingthings/animals/22da618d").set({ name: "aardvark" })
+        const startAfterDoc = await db
+            .doc("livingthings/animals/22da618d")
+            .get()
+
+        // When we order the collection by the document id;
+        const result = await db
+            .collectionGroup("animals")
+            .orderBy(FieldPath.documentId())
+            .startAfter(startAfterDoc)
+            .get()
+
+        // Then we should get no items
+        expect(result.size).toEqual(0)
+    })
 })
